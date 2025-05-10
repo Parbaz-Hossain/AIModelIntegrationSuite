ac144b55de05530e57faeea932ab5ba7d9e39b86
md.parvezcse@gmail.com <md.parvezcse@gmail.com>
2025-05-10 22:10:01 +0600

    OpenAI API, Ollama integration

---
+++ AIModelIntegration.Ollama/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+Console.Title = "OpenAI Ollama Chat Assistant 🤖";
+
+var builder = Kernel.CreateBuilder();
+
+// Suppress the diagnostic warning SKEXP0070 by explicitly acknowledging it
+#pragma warning disable SKEXP0070
+builder.AddOllamaChatCompletion("llama3.2:1b", new Uri("http://localhost:11434"));
+#pragma warning restore SKEXP0070
+
+var kernel = builder.Build();
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+history.AddSystemMessage("You are a helpful assistant.");
+
+while (true)
+{
+    Console.Write("Enter your message: ");
+    var userMessage = Console.ReadLine();
+    Console.WriteLine("Thinking...");
+    if (string.IsNullOrWhiteSpace(userMessage))
+        break;
+
+    history.AddUserMessage(userMessage);
+    var response = await chatService.GetChatMessageContentAsync(history);
+    Console.WriteLine($"AI Agent: {response.Content}");
+}
---
+++ AIModelIntegration.OpenAI/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI.Chat;
+
+Console.Title = "OpenAI Chat Client 🤖";
+
+ChatClient chatClient = new(
+   model: "gpt-4o-mini",
+   apiKey: Environment.GetEnvironmentVariable("OpenAI_API_Key"));
+
+while (true)
+{
+    Console.WriteLine("Enter your message (or type 'exit' to quit):");
+
+    var userPrompt = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(userPrompt) || userPrompt.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+        break;
+
+    Console.WriteLine("Thinking...");
+
+    await foreach (var message in chatClient.CompleteChatStreamingAsync(userPrompt))
+    {
+        foreach (var item in message.ContentUpdate)
+        {
+            Console.Write(item.Text);
+        }
+    }
+
+    Console.WriteLine();
+}
+
